b50843abb2b8278ec484299178352e410f4ca415
Jeremy Mann <jmann277@gmail.com>
2022-02-20 13:35:07 -0800

    capitalize minor/major

--- tests/tarot_spec.lua
+++ tests/tarot_spec.lua
@@ -23,7 +23,7 @@ describe("tarot", function()
     local cards = require("tarot").get_tarots()
     for _, card_stuff in pairs(cards) do
       local arcana_type = card_stuff.arcana_type
-      local is_valid = (arcana_type == "major") or (arcana_type == "minor")
+      local is_valid = (arcana_type == "Major") or (arcana_type == "Minor")
       assert(is_valid)
     end
   end)
